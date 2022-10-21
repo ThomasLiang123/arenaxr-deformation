9a9db78dacaf7c16a6f2b907cd04dd59de217914
Thomas Liang <tliang1850@gmail.com>
2022-10-21 01:23:09 -0500

    improved grid deformation

--- grid-deformation.py
+++ grid-deformation.py
@@ -1,111 +1,121 @@
 from arena import *
+import math
 
 scene = Scene(host="mqtt.arenaxr.org", scene="grid-deformation")
 
-GRID_WIDTH = 5
-GRID_LENGTH = 5
+GRID_WIDTH = 20
+GRID_LENGTH = 20
 
-defidx = 0
+DEFORM_RADIUS = 2
+MAX_DEFORM = 0.5
 
-def reset_grid():
+lasti = 0
+lastj = 0
+
+def setMorphs(i, j, m1, m2, m3, m4):
     global grid
     global morphs
 
-    morphs[0].value = 0
-    morphs[1].value = 0
-    morphs[2].value = 0
-    morphs[3].value = 0
+    if (i < 0 or j < 0 or i >= GRID_LENGTH or j >= GRID_WIDTH):
+        return
 
-    for i in range(GRID_WIDTH):
-        for j in range(GRID_LENGTH):
-            g = grid[i][j]
+    g = grid[i][j]
+    morphs[0].value = m1
+    morphs[1].value = m2
+    morphs[2].value = m3
+    morphs[3].value = m4
+    g.update_morph(morphs)
+    scene.update_object(g)
 
-            g.update_morph(morphs)
-            scene.update_object(g)
+def reset_at(i, j):
+    global grid
+    global morphs
+
+    for ii in range(i-DEFORM_RADIUS, i+DEFORM_RADIUS+1):
+        for jj in range(j-DEFORM_RADIUS, j+DEFORM_RADIUS+1):
+            setMorphs(ii,jj,0,0,0,0)
+
+def deform_func(x):
+
+    return MAX_DEFORM / (1 + math.exp((8*MAX_DEFORM)*x - (4*MAX_DEFORM)))
+
 def deform_grid(i, j):
     global grid
     global morphs
 
-    g0 = grid[i][j]
-    morphs[0].value = 0.5
-    morphs[1].value = 0.5
-    morphs[2].value = 0.5
-    morphs[3].value = 0.5
-    g0.update_morph(morphs)
-    scene.update_object(g0)
+    for ii in range(i-DEFORM_RADIUS, i+DEFORM_RADIUS+1):
+        for jj in range(j-DEFORM_RADIUS, j+DEFORM_RADIUS+1):
+            idiff = float(ii - i)
+            jdiff = float(jj - j)
 
-    if i > 0:
-        gi = grid[i-1][j]
-        morphs[0].value = 0.5
-        morphs[1].value = 0.5
-        morphs[2].value = 0
-        morphs[3].value = 0
-        gi.update_morph(morphs)
-        scene.update_object(gi)
+            idefclose, ideffar, jdefclose, jdeffar = 0.0,0.0,0.0,0.0
 
-    if i < GRID_WIDTH-1:
-        gi = grid[i+1][j]
-        morphs[0].value = 0
-        morphs[1].value = 0
-        morphs[2].value = 0.5
-        morphs[3].value = 0.5
-        gi.update_morph(morphs)
-        scene.update_object(gi)
+            if idiff < 0:
+                idefclose = deform_func(abs(idiff)-1)
+                ideffar = deform_func(abs(idiff))
+            elif idiff > 0:
+                ideffar = deform_func(abs(idiff)-1)
+                idefclose = deform_func(abs(idiff))
+            else:
+                idefclose = deform_func(abs(idiff))
+                ideffar = deform_func(abs(idiff))
 
-    if j > 0:
-        gi = grid[i][j-1]
-        morphs[0].value = 0.5
-        morphs[1].value = 0
-        morphs[2].value = 0.5
-        morphs[3].value = 0
-        gi.update_morph(morphs)
-        scene.update_object(gi)
+            if jdiff < 0:
+                jdefclose = deform_func(abs(jdiff)-1)
+                jdeffar = deform_func(abs(jdiff))
+            elif jdiff > 0:
+                jdeffar = deform_func(abs(jdiff)-1)
+                jdefclose = deform_func(abs(jdiff))
+            else:
+                jdefclose = deform_func(abs(jdiff))
+                jdeffar = deform_func(abs(jdiff))
 
-    if j < GRID_LENGTH-1:
-        gi = grid[i][j+1]
-        morphs[0].value = 0
-        morphs[1].value = 0.5
-        morphs[2].value = 0
-        morphs[3].value = 0.5
-        gi.update_morph(morphs)
-        scene.update_object(gi)
+            corner1 = idefclose+jdefclose
+            corner2 = idefclose+jdeffar
+            corner3 = ideffar+jdefclose
+            corner4 = ideffar+jdeffar
 
-    if i > 0 and j > 0:
-        gi = grid[i-1][j-1]
-        morphs[0].value = 0.5
-        morphs[1].value = 0
-        morphs[2].value = 0
-        morphs[3].value = 0
-        gi.update_morph(morphs)
-        scene.update_object(gi)
+            if idiff == -DEFORM_RADIUS:
+                corner3 = 0
+                corner4 = 0
+            elif idiff == DEFORM_RADIUS:
+                corner1 = 0
+                corner2 = 0
 
-    if i > 0 and j < GRID_LENGTH-1:
-        gi = grid[i-1][j+1]
-        morphs[0].value = 0
-        morphs[1].value = 0.5
-        morphs[2].value = 0
-        morphs[3].value = 0
-        gi.update_morph(morphs)
-        scene.update_object(gi)
+            if jdiff == -DEFORM_RADIUS:
+                corner2 = 0
+                corner4 = 0
+            elif jdiff == DEFORM_RADIUS:
+                corner1 = 0
+                corner3 = 0
 
-    if i < GRID_WIDTH-1 and j > 0:
-        gi = grid[i + 1][j - 1]
-        morphs[0].value = 0
-        morphs[1].value = 0
-        morphs[2].value = 0.5
-        morphs[3].value = 0
-        gi.update_morph(morphs)
-        scene.update_object(gi)
+            setMorphs(ii,jj,corner1,corner2,corner3,corner4)
 
-    if i < GRID_WIDTH-1 and j < GRID_LENGTH - 1:
-        gi = grid[i + 1][j + 1]
-        morphs[0].value = 0
-        morphs[1].value = 0
-        morphs[2].value = 0
-        morphs[3].value = 0.5
-        gi.update_morph(morphs)
-        scene.update_object(gi)
-        
+    # setMorphs(i,j,0.5,0.5,0.5,0.5)
+    # setMorphs(i-1,j,0.5,0.5,0,0)
+    # setMorphs(i+1,j,0,0,0.5,0.5)
+    # setMorphs(i,j-1,0.5,0,0.5,0)
+    # setMorphs(i,j+1,0,0.5,0,0.5)
+    # setMorphs(i-1,j-1,0.5,0,0,0)
+    # setMorphs(i-1,j+1,0,0.5,0,0)
+    # setMorphs(i+1,j-1,0,0,0.5,0)
+    # setMorphs(i+1,j+1,0,0,0,0.5)
+
+def click(scene, evt, msg):
+    global lasti
+    global lastj
+
+    if (evt.type == "mousedown"):
+        strs = evt.object_id.split('-')
+        i = int(strs[2])
+        j = int(strs[3])
+
+        reset_at(lasti, lastj)
+        deform_grid(i, j)
+        lasti = i
+        lastj = j
+    elif (evt.type == "mouseup"):
+        reset_at(lasti, lastj)
 
 @scene.run_once
 def make_grid():
@@ -122,11 +132,13 @@ def make_grid():
         row = []
         for j in range(GRID_LENGTH):
             item = GLTF(
-                object_id="deform-cube"+str(i)+str(j),
+                object_id="deform-cube-"+str(i)+"-"+str(j),
                 position=(i,0.5,j),
                 scale=(0.5,0.5,0.5),
                 action="create",
-                persist=False,
+                persist=True,
+                clickable=True,
+                evt_handler=click,
                 url="store/users/thomasliang/griditem.glb",
             )
             item.update_morph(morphs)
@@ -134,17 +146,5 @@ def make_grid():
             row.append(item)
 
         grid.append(row)
-
-
-@scene.run_forever(interval_ms=1000)
-def periodic():
-    global defidx
-
-    defidx = (defidx + 1) % (GRID_WIDTH * GRID_LENGTH)
-    defi = int(defidx / GRID_LENGTH)
-    defj = int(defidx % GRID_LENGTH)
-
-    reset_grid()s
-    deform_grid(defi, defj)
 
 scene.run_tasks()
